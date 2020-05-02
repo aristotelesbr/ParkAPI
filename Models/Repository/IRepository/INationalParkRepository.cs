c47413909e072a9cabd381fe8f09eb5b7f1e4c9a
Aristoteles <aristotelesbr2014@gmail.com>
2020-05-02 08:26:15 -0300

    Fix classes name

--- Models/Repository/IRepository/INationalParkRepository.cs
+++ Models/Repository/IRepository/INationalParkRepository.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace ParkyAPI.Models.Repository.IRepository
 {
-    public interface INationalRepository
+    public interface INationalParkRepository
     {
         ICollection<NationalPark> GetNationalParks();
 
